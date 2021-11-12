084fe5e46af9f840a666492b7c90c76925c6095e
haris021 <harismushtaq21@yahoo.com>
2021-11-12 16:30:43 +0500

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ fig.add_trace(go.Scatter(x=Inflow_Data['Time'], y=Inflow_Data[flow_river],showle
 fig.add_trace(go.Scatter(x=reservior_data['Time'], y=reservior_data[level_reservoir],showlegend = False),row=2, col=1)
 fig.add_trace(go.Scatter(name ='Today',x=barage_df['Time'], y=barage_df['Today']), row=2, col=2)
 fig.add_trace(go.Scatter(name ='Last Year',x=barage_df['Time'], y=barage_df['Last Year']),row=2, col=2)
-fig.update_layout(height=800, width=700, showlegend=True, title_text="Pakistan Flow Analytics", legend = dict(yanchor="top",y=0.45))
+fig.update_layout(height=800, width=850, showlegend=True, title_text="Pakistan Flow Analytics", legend = dict(yanchor="top",y=0.45))
 st.write(fig)
 
 # download = st.button('Download Shape File')
