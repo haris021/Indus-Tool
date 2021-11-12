a27de133d651d9ba496ac221addc9b84c6190580
haris021 <harismushtaq21@yahoo.com>
2021-11-12 15:46:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ rivers = ['Indus','Jhelum', 'Chenab', 'Kabul']
 river_ops = st.sidebar.radio("River Inflow plot", rivers)
 
 lev_rivers = ['Indus','Jhelum']
-river_level = st.sidebar.radio("River level plot", lev_rivers)
+reservoir_level = st.sidebar.radio("Reservoir level plot", lev_rivers)
 
 barages = ['Guddu','Sukkur','Kotri']
 selected_barage = st.sidebar.radio("Barage", barages)
