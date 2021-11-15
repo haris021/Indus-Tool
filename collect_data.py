06f2910f17372c8ee46b89780acaf7d8474d84b3
haris021 <harismushtaq21@yahoo.com>
2021-11-16 03:03:11 +0500

    Update collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -28,7 +28,7 @@ def table_to_csv(table_data, headings,out_file):
 		data.append(sub_data)
 
 	df = pd.DataFrame(data, columns=headings)
-	df.to_csv(out_file)
+	df.to_excel(out_file)
 
 
 def get_river_flow_data():
@@ -39,7 +39,7 @@ def get_river_flow_data():
 	Headings = ['Time', 'Indus_levels','Indus_Inflow','Indus_Outflow','Kabul_Inflow',
 	'Jhelum_levels','Jhelum_Inflow','Jhelum_Outflow', 'Chenab_Inflow','System_Inflow_now',
 	'System_Inflow_past','System_Inflow_avg']
-	file = 'https://github.com/haris021/Indus-Tool/edit/main/river_flow_data.csv'
+	file = 'https://github.com/haris021/Indus-Tool/edit/main/river_flow_data.xlsx'
 	table_to_csv(table_data,headings = Headings, out_file = file)
 
 
@@ -53,8 +53,8 @@ def get_sindh_barage_data():
 	date = time_arr[1]
 	print(date)
 	sindh_data = table_data[25:28]
-	out = 'https://github.com/haris021/Indus-Tool/edit/main/Sindh_barage.csv'
-	df = pd.read_csv(out)
+	out = 'https://github.com/haris021/Indus-Tool/edit/main/Sindh_barage.xlsx'
+	df = pd.read_excel(out)
 	final_data = []
 	for row in sindh_data:
 		row_data = row.find_all('td')
@@ -72,7 +72,7 @@ def get_sindh_barage_data():
 	df['Time'] = pd.to_datetime(df['Time'])
 	print(df)
 	df = df.drop_duplicates()
-	df.to_csv(out, index=False)
+	df.to_excel(out, index=False)
 
 
 get_river_flow_data()
