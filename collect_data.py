96021820fe0b4506cfff921e0301d2c509e08dbe
haris021 <harismushtaq21@yahoo.com>
2021-11-18 11:03:40 +0500

    Update collect_data.py

--- collect_data.py
+++ collect_data.py
@@ -53,10 +53,9 @@ def get_sindh_barage_data():
 	time_arr = time.split(" ")
 	date = time_arr[1]
 	print(date)
-	out ='https://github.com/haris021/Indus-Tool/edit/main/Sindh_barage.xlsx'
-	df = pd.read_excel(out)
-	#df = pd.read_csv(out,sep='\t')
 	sindh_data = table_data[25:28]
+	out = 'https://github.com/haris021/Indus-Tool/edit/main/Sindh_barage.xlsx'
+	df = pd.read_excel(out)
 	final_data = []
 	for row in sindh_data:
 		row_data = row.find_all('td')
@@ -66,16 +65,15 @@ def get_sindh_barage_data():
 			sub_data.append(entry)
 		print(sub_data)
 		final_data.append(sub_data)
+	print('df before append ........')
+	print(df)
 	df2 = pd.DataFrame(final_data, columns=caps)
+	print(df2)
 	df = df.append(df2, ignore_index=True)
-	#out ='https://github.com/haris021/Indus-Tool/edit/main/Sindh_barage.xlsx'
-	#df = pd.read_excel(out)
-	#table_to_csv(final_data,headings = caps, out_file = out)
-	#df2.to_excel(out, startrow=len(dff)+1, header=False, index=False)
 	df['Time'] = pd.to_datetime(df['Time'])
 	print(df)
 	df = df.drop_duplicates()
-	df.to_excel(out)
+	df.to_excel(out, index=False)
 	
 
 
